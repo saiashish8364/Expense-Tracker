2a86dfe555ed5650690a7c3cce24ab5f5586527d
saiashish8364 <polusaiashish97@gmail.com>
2023-07-12 19:30:23 +0530

    expense7

--- expensetracker/src/Components/PasswordReset/PassReset.js
+++ expensetracker/src/Components/PasswordReset/PassReset.js
@@ -1,9 +1,11 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function PassReset() {
+  const [loading, setLoading] = useState(false);
   const emailInputRef = useRef();
   const resetHandler = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch(
         "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBCCfgwCyOwJVyOt98XoUeYffa0QBCV_Yk",
@@ -20,6 +22,7 @@ function PassReset() {
       );
       if (response.ok) {
         alert("Reset link sent to mail");
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -27,33 +30,48 @@ function PassReset() {
   };
   return (
     <>
-      <form
-        style={{
-          marginTop: "10%",
-          justifyContent: "center",
-          marginLeft: "33%",
-          fontSize: "1.15rem",
-        }}
-      >
-        <label>Email:</label>
-        <input style={{ marginLeft: "10px" }} ref={emailInputRef} />
-        <br />
-        <br />
-        <button
+      {!loading && (
+        <form
           style={{
-            marginLeft: "13.5%",
-            height: "30px",
-            width: "17.5%",
-            fontSize: "1rem",
-            backgroundColor: "black",
-            color: "white",
-            borderRadius: "25px",
+            marginTop: "10%",
+            justifyContent: "center",
+            marginLeft: "33%",
+            fontSize: "1.15rem",
           }}
-          onClick={resetHandler}
         >
-          Submit
-        </button>
-      </form>
+          <label>Email:</label>
+          <input style={{ marginLeft: "10px" }} ref={emailInputRef} />
+          <br />
+          <br />
+          <button
+            style={{
+              marginLeft: "13.5%",
+              height: "30px",
+              width: "17.5%",
+              fontSize: "1rem",
+              backgroundColor: "black",
+              color: "white",
+              borderRadius: "25px",
+            }}
+            onClick={resetHandler}
+          >
+            Submit
+          </button>
+        </form>
+      )}
+
+      {loading && (
+        <h1
+          style={{
+            marginTop: "10%",
+            justifyContent: "center",
+            marginLeft: "38.5%",
+            fontSize: "2.5rem",
+          }}
+        >
+          Loading...
+        </h1>
+      )}
     </>
   );
 }
