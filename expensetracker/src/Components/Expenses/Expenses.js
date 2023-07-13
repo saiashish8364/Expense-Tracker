485339e4cd0fffcbede06785cd29b1f4d61f65f3
saiashish8364 <polusaiashish97@gmail.com>
2023-07-13 17:23:02 +0530

    expense10

--- expensetracker/src/Components/Expenses/Expenses.js
+++ expensetracker/src/Components/Expenses/Expenses.js
@@ -1,37 +1,33 @@
 import { useEffect, useRef, useState } from "react";
-let uniqueIds = [];
+
 const Expenses = () => {
   const [expenses, setExpenses] = useState([]);
   const priceInputRef = useRef();
   const descriptionInputRef = useRef();
   const categoryInputref = useRef();
-
-  useEffect(() => {
-    async function fetchData() {
-      let t = String(localStorage.getItem("email"));
-      try {
-        const response = await fetch(
-          `https://expense-tracker-sharpner-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${t}.json`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          for (let key in data) {
-            if (!uniqueIds.includes(key)) {
-              uniqueIds.push(key);
-              let cpy = {
-                id: key,
-                category: data[key].category,
-                description: data[key].description,
-                price: data[key].price,
-              };
-              setExpenses((prev) => [...prev, cpy]);
-            }
-          }
+  async function fetchData() {
+    let t = String(localStorage.getItem("email"));
+    try {
+      const response = await fetch(
+        `https://expense-tracker-sharpner-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${t}.json`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        for (let key in data) {
+          let cpy = {
+            id: key,
+            category: data[key].category,
+            description: data[key].description,
+            price: data[key].price,
+          };
+          setExpenses((prev) => [...prev, cpy]);
         }
-      } catch (error) {
-        console.log(error);
       }
+    } catch (error) {
+      console.log(error);
     }
+  }
+  useEffect(() => {
     fetchData();
   }, []);
   async function expenseSubmitHandler(e) {
@@ -51,18 +47,47 @@ const Expenses = () => {
         }
       );
       if (response.ok) {
+        let d = await response.text().json();
+        const data1 = {
+          id: d.name,
+          price: priceInputRef.current.value,
+          description: descriptionInputRef.current.value,
+          category: categoryInputref.current.value,
+        };
+        setExpenses([...expenses, data1]);
+
+        console.log(d);
         alert("Expense Added.");
       }
     } catch (error) {
       console.log(error);
     }
 
-    setExpenses([...expenses, data]);
     priceInputRef.current.value = "";
     descriptionInputRef.current.value = "";
     categoryInputref.current.value = "";
   }
-
+  const expenseDeleteHandler = async (e) => {
+    try {
+      let em = String(localStorage.getItem("email"));
+      let id = String(e.target.id);
+      console.log(String(e.target.id));
+      const response = await fetch(
+        `https://expense-tracker-sharpner-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${em}/${id}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        console.log("deleted");
+      } else {
+        console.error("Failed to delete data");
+      }
+    } catch (error) {
+      console.error("Error deleting data:", error);
+    }
+  };
+  const expenseEditHandler = (e) => {};
   return (
     <>
       <section
@@ -129,6 +154,8 @@ const Expenses = () => {
               <th>Expense Price</th>
               <th>Expense Description</th>
               <th>Expense Category</th>
+              <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -138,6 +165,16 @@ const Expenses = () => {
                   <td style={{ justifyContent: "center" }}>Rs.{ex.price}</td>
                   <td style={{ justifyContent: "center" }}>{ex.description}</td>
                   <td style={{ justifyContent: "center" }}>{ex.category}</td>
+                  <td>
+                    <button id={ex.id} onClick={expenseEditHandler}>
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button id={ex.id} onClick={expenseDeleteHandler}>
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               );
             })}
